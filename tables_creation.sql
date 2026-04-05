90a944128abccb9cb824ea9d9b7749f3b312abfc
kaoschoupie <maxime9212@gmail.com>
2026-04-05 11:53:41 -0400

    corrected issue where FROM and WHERE were all lower case, figured the payment type problem?

--- tables_creation.sql
+++ tables_creation.sql
@@ -9,9 +9,9 @@ CREATE TABLE Customer_type(
 	CustomerID CHAR(10) NOT NULL,
 	Type CHAR(1)NOT NULL,
 	PRIMARY KEY(CustomerID),
-	FOREIGN KEY CustomerID REFERENCES Customer(CustomerID),
+	FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
 	CHECK(Type IN(‘0’,’1’)),
-	CHECK(CustomerID IN(SELECT CustomerID from Customer)),
+	CHECK(CustomerID IN(SELECT CustomerID FROM Customer)),
 );
 CREATE TABLE Weight_class(
 	Class VARCHAR(16), NOT NULL,
@@ -22,8 +22,8 @@ CREATE TABLE Driver(
 	DriverID VARCHAR(16) NOT NULL,
 	LicenseClass VARCHAR(10) NOT NULL,
 	PRIMARY KEY(DriverID),
-	FOREIGN KEY LicenseClass REFERENCES Weight_class(Class),
-	CHECK(LicenseClass IN(SELECT Class from Weight_class))
+	FOREIGN KEY (LicenseClass) REFERENCES Weight_class(Class),
+	CHECK(LicenseClass IN(SELECT Class FROM Weight_class))
 	,
 );
 CREATE TABLE Vehicle(
@@ -31,8 +31,8 @@ CREATE TABLE Vehicle(
 	OdometerMiles INT NOT NULL,
 	VehicleClass VARCHAR(10) NOT NULL,
 	PRIMARY KEY(VehicleID),
-	FOREIGN KEY VehicleClass REFERENCES Weight_class(Class),
-	CHECK(VehicleClass IN(SELECT Class from Weight_class)),
+	FOREIGN KEY (VehicleClass) REFERENCES Weight_class(Class),
+	CHECK(VehicleClass IN(SELECT Class FROM Weight_class)),
 );
 CREATE TABLE Reservation(
 	ReservationID CHAR(16) NOT NULL,
@@ -44,10 +44,10 @@ CREATE TABLE Reservation(
 	TypeOfVehicle VARCHAR(10) NOT NULL,
 	CustomerID CHAR(10) NOT NULL,
 	PRIMARY KEY(ReservationID),
-	FOREIGN KEY TypeOfVehicle REFERENCES Weight_class(Class),
-	FOREIGN KEY CustomerID REFERENCES Customer(CustomerID),
-	CHECK(TypeOfVehicle IN(SELECT Class from Weight_class)),
-	CHECK(CustomerID IN(SELECT CustomerID from Customer)),
+	FOREIGN KEY (TypeOfVehicle) REFERENCES Weight_class(Class),
+	FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
+	CHECK(TypeOfVehicle IN(SELECT Class FROM Weight_class)),
+	CHECK(CustomerID IN(SELECT CustomerID FROM Customer)),
 CHECK (AppointmentDate > DateOfBooking
   AND AppointmentDate <= DateOfBooking + INTERVAL 1 YEAR)
 );
@@ -56,10 +56,10 @@ CREATE TABLE Rental(
 	CustomerID CHAR(10) NOT NULL,
 	ReservationID CHAR(16) NOT NULL,
 	PRIMARY KEY(RentalID),
-	FOREIGN KEY CustomerID REFERENCES Customer(CustomerID),
-	FOREIGN KEY ReservationID REFERENCES Reservation(ReservationID),
-	CHECK(CustomerID IN(SELECT CustomerID from Customer)),
-	CHECK(ReservationID IN(SELECT ReservationID from Reservation)),
+	FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
+	FOREIGN KEY (ReservationID) REFERENCES Reservation(ReservationID),
+	CHECK(CustomerID IN(SELECT CustomerID FROM Customer)),
+	CHECK(ReservationID IN(SELECT ReservationID FROM Reservation)),
 );
 CREATE TABLE Mission(
 	MissionID CHAR(10) NOT NULL,
@@ -73,12 +73,12 @@ CREATE TABLE Mission(
 	DriverID VARCHAR(16) NOT NULL,
 	VehicleID VARCHAR(16) NOT NULL,
 	PRIMARY KEY(MissionID),
-	FOREIGN KEY RentalID REFERENCES Rental(RentalID),
-	FOREIGN KEY DriverID REFERENCES Driver(DriverID),
-	FOREIGN KEY VehicleID REFERENCES Vehicle(VehicleID),
-	CHECK(RentalID IN(SELECT RentalID from Rental)),
-	CHECK(DriverID IN(SELECT DriverID from Driver)),
-	CHECK(VehicleID IN(SELECT VehicleID from Vehicle)),
+	FOREIGN KEY (RentalID) REFERENCES Rental(RentalID),
+	FOREIGN KEY (DriverID) REFERENCES Driver(DriverID),
+	FOREIGN KEY (VehicleID) REFERENCES Vehicle(VehicleID),
+	CHECK(RentalID IN(SELECT RentalID FROM Rental)),
+	CHECK(DriverID IN(SELECT DriverID FROM Driver)),
+	CHECK(VehicleID IN(SELECT VehicleID FROM Vehicle)),
 	CHECK(EndDay>StartDay OR (EndDay=StartDay AND EndTime>StartTime)),
 	CHECK(EndTime<='18:00:00'),
 	CHECK(StartTime>=’8:00:00’),
@@ -88,8 +88,8 @@ CREATE TABLE Sheet(
 	SheetID CHAR(10) NOT NULL,
 	MissionID CHAR(10) NOT NULL,
 	PRIMARY KEY(SheetID),
-	FOREIGN KEY MissionID REFERENCES Mission(MissionID),
-	CHECK(MissionID IN(SELECT MissionID from Mission)),
+	FOREIGN KEY (MissionID) REFERENCES Mission(MissionID),
+	CHECK(MissionID IN(SELECT MissionID FROM Mission)),
 );
 CREATE TABLE Invoice(
 	InvoiceID CHAR(10) NOT NULL,
@@ -97,8 +97,8 @@ CREATE TABLE Invoice(
 	TotalAmount DECIMAL(65,2) NOT NULL,
 	CustomerID char(10) NOT NULL,
 	PRIMARY KEY(InvoiceID),
-	FOREIGN KEY CustomerID REFERENCES Customer(CustomerID),
-	CHECK(CustomerID IN(SELECT CustomerID from Customer)),
+	FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
+	CHECK(CustomerID IN(SELECT CustomerID FROM Customer)),
 )
 CREATE TABLE Invoice_line(
 	InvoiceID CHAR(10) NOT NULL,
@@ -106,48 +106,56 @@ CREATE TABLE Invoice_line(
 	DurationCost DECIMAL(65,2) NOT NULL,
 	MileageCost DECIMAL(65,2) NOT NULL,
 	PRIMARY KEY(InvoiceID,MissionID),
-	FOREIGN KEY InvoiceID REFERENCES Invoice(InvoiceID),
-	FOREIGN KEY MissionID REFERENCES Mission(MissionID),
-	CHECK(InvoiceID IN(SELECT InvoiceID from Invoice)),
-	CHECK(MissionID IN(SELECT MissionID from Mission)),
+	FOREIGN KEY (InvoiceID) REFERENCES Invoice(InvoiceID),
+	FOREIGN KEY (MissionID) REFERENCES Mission(MissionID),
+	CHECK(InvoiceID IN(SELECT InvoiceID FROM Invoice)),
+	CHECK(MissionID IN(SELECT MissionID FROM Mission)),
 )
 CREATE TABLE Invoice_payment(
 	PaymentID CHAR(10) NOT NULL,
 	Amount DECIMAL(65,2) NOT NULL,
-	PaymentType##
+	PaymentType VARCHAR(10) NOT NULL,
 	PaymentDate DATE NOT NULL,
 	InvoiceID CHAR(10) NOT NULL,
 	CustomerID CHAR(10) NOT NULL,
 	PRIMARY KEY(PaymentID),
-	FOREIGN KEY InvoiceID REFERENCES Invoice(InvoiceID),
-	FOREIGN KEY CustomerID REFERENCES Customer(CustomerID),
-	CHECK(InvoiceID IN(SELECT InvoiceID from Invoice)),
-	CHECK(MissionID IN(SELECT MissionID from Mission)),
-	CHECK(Amount = (SELECT TotalAmount from Invoice where invoiceID=Invoice.InvoiceID)),
+	FOREIGN KEY (InvoiceID) REFERENCES Invoice(InvoiceID),
+	FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
+	CHECK(InvoiceID IN(SELECT InvoiceID FROM Invoice)),
+	CHECK(MissionID IN(SELECT MissionID FROM Mission)),
+	CHECK(Amount = (SELECT TotalAmount FROM Invoice WHERE invoiceID=Invoice.InvoiceID)),
+	CHECK(PaymentType IN (SELECT PaymentType FROM ))
 )
 CREATE TABLE Payment_type(
-	PaymentType CHAR(10) NOT NULL,
+	PaymentType VARCHAR(10) NOT NULL,
 	PRIMARY KEY(PaymentType),
 	CONSTRAINT Allowed-Types CHECK(PaymentType IN('cash','credit','check')),
 )
-CREATE TABLE Type_link(
+CREATE TABLE Payment_instance(
 	TypeID VARCHAR(16) NOT NULL,
 	PaymentType CHAR(10) NOT NULL,
 	PRIMARY KEY(TypeID),
-	FOREIGN KEY PaymentType REFERENCES Payment_type(PaymentType),
+	FOREIGN KEY (PaymentType) REFERENCES Payment_type(PaymentType),
 	CHECK (PaymentType IN (SELECT PaymentType FROM Payment_type)),
 )
 CREATE TABLE Cash_type(
-
+	TypeID VARCHAR(16) NOT NULL,
+	PRIMARY KEY(TypeID),
+	FOREIGN KEY (typeID) REFERENCES Payment_instance(TypeID),
 )
 CREATE TABLE Credit_card_type(
-	CardID CHAR(16) NOT NULL,
+	TypeID VARCHAR(16) NOT NULL,
+	CardID CHAR(16) NOT NULL UNIQUE,
 	CardName VARCHAR(32) NOT NULL,
 	ExpirationDate DATE NOT NULL,
+	PRIMARY KEY(CardID),
+	FOREIGN KEY (typeID) REFERENCES Payment_instance(TypeID),
 )
 CREATE TABLE Check_type(
+	TypeID VARCHAR(16) NOT NULL,
 	CheckNumber VARCHAR(8) NOT NULL,
 	SenderBankNb VARCHAR(16) NOT NULL,
 	ReceiverName CHAR(8) NOT NULL,
 	PRIMARY KEY(CheckNumber,SenderBankNb),
+	FOREIGN KEY (typeID) REFERENCES Payment_instance(TypeID),
 )
