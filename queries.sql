8878f0dca6e89da11edf40c735a2e1d8533fd9fe
kaoschoupie <maxime9212@gmail.com>
2026-04-06 10:49:22 -0400

    Queries a,b,c from project specs

--- queries.sql
+++ queries.sql
@@ -22,3 +22,19 @@ JOIN Driver d ON m.DriverID = d.DriverID;
 SELECT i.InvoiceID, ip.Amount, ip.PaymentType
 FROM Invoice i
 JOIN Invoice_payment ip ON i.InvoiceID = ip.InvoiceID;
+
+-- a) List of customers that are businesses
+SELECT c.CustomerID 
+FROM Customer c,Customer_type t 
+WHERE c.CustomerID=t.CustomerID AND t.Type='1'
+
+-- b) List of reservations whose reservation number is greater than 1(meaning which reservations have more than 1 rental)
+SELECT r.ReservationID
+FROM Reservation AS r
+JOIN Rental AS t ON r.ReservationID = t.ReservationID
+GROUP BY r.ReservationID
+HAVING COUNT(t.RentalID) > 1;
+
+-- c) List of drivers and vehicles having participated in at least one mission
+SELECT DISTINCT DriverID,VehicleID
+FROM Mission
